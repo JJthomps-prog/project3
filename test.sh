fe9e9b84a30d92a7f6650adafc864f7e3d02437c
ZhengyuanHan <129567395+ZhengyuanHan@users.noreply.github.com>
2023-04-28 21:23:56 -0400

    Add files via upload

--- test.sh
+++ test.sh
@@ -9,3 +9,6 @@ PID=$! # record the PID
 newman run forum_multiple_posts.postman_collection.json -e env.json # use the env file
 newman run forum_post_read_delete.postman_collection.json -n 50 # 50 iterations
 newman run search.json
+newman run user.postman_collection.json -e user_info.postman_environment.json
+newman run User-based_range_queries.postman_collection.json -e user_info.postman_environment.json
+newman run fulltext_search.postman_collection.json -e user_info.postman_environment.json
